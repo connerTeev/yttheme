84bd03bcd4205c9c696bb27949dee08ff79716ce
connerTeev <connerteevan@gmail.com>
2024-01-28 12:52:00 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package Pluto
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pluto Theme</title>
+</head>
+<body>
+  
+</body>
+</html>
